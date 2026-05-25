3d06a6520acaf84fc433db98bdb456036942e55c
yaswanthsribalachandra <22331A4415@mvgrce.edu.in>
2026-05-25 21:12:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,29 +4,42 @@ import joblib
 import numpy as np
 
 # Create FastAPI app
-app = FastAPI()
+app = FastAPI(title="ML Model API")
 
 # Load model
 model = joblib.load("model.pkl")
+
 
 # Request schema
 class InputData(BaseModel):
     number: float
 
+
 # Health check
 @app.get("/")
 def home():
-    return {"message": "ML Model API Running"}
+    return {
+        "message": "ML Model API Running"
+    }
+
 
 # Prediction endpoint
 @app.post("/predict")
 def predict(data: InputData):
 
-    input_value = np.array([[data.number]])
+    try:
+        # Convert input into numpy array
+        input_value = np.array([[data.number]])
 
-    prediction = model.predict(input_value)
+        # Model prediction
+        prediction = model.predict(input_value)
 
-    return {
-        "input": data.number,
-        "prediction": prediction[0]
-    }
+        return {
+            "input": data.number,
+            "prediction": float(prediction[0])
+        }
+
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
